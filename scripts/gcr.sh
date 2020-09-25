4e038400f45266f5b9a93c4cd14a3ba254ec11e2
Samuel Liu <liupeng0518@gmail.com>
2020-09-25 11:18:54 +0800

    fix shell

--- scripts/gcr.sh
+++ scripts/gcr.sh
@@ -4,7 +4,8 @@ kube_image_repo=k8s.gcr.io
 docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWD
 
 for list in `cat k8s.list|grep -v ^$|grep -v ^#`; do
-  list=`echo $list | sed 's/\//./g'`
+  rel_list=`echo $list | sed 's/\//./g'`
   docker pull $kube_image_repo/$list
-  docker tag $kube_image_repo/$list
+  docker tag $kube_image_repo/$list $DOCKERHUB_USERNAME/$rel_list
+  docker push $DOCKERHUB_USERNAME/$rel_list
 done

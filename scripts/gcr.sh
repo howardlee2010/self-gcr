8b8b6a8c1ace5ee75899acb14a4ffb903983b9d8
Samuel Liu <liupeng0518@gmail.com>
2021-11-10 20:05:09 +0800

    Update gcr.sh

--- scripts/gcr.sh
+++ scripts/gcr.sh
@@ -2,7 +2,9 @@
 # 
 kube_image_repo=k8s.gcr.io
 docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWD
+echo "test"
 docker buildx build --platform linux/arm64 -t liupeng0518/pip:latest --push .
+
 for list in `cat k8s.list|grep -v ^$|grep -v ^#`; do
   rel_list=`echo $list | sed 's/\//./g'`
   docker pull $kube_image_repo/$list

6a69c3ffeee5e491b6ed1d744b6e7973addd7e05
howardlee2010 <howard@mail.ustc.edu.cn>
2024-05-23 16:35:59 +0800

    Update k8s-build-images-get.sh

--- scripts/k8s-build-images-get.sh
+++ scripts/k8s-build-images-get.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-#
+
 set -x
 
 kube_image_repo="registry.k8s.io/build-image"
@@ -8,7 +8,11 @@ docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWD
 
 for baseimage in `cat k8s-build-images | grep -v ^$ | grep -v ^#`; do
     echo ${baseimage}
-    docker pull ${kube_image_repo}/${baseimage}
-    docker tag ${kube_image_repo}/${baseimage} $DOCKERHUB_USERNAME/${baseimage}
-    docker push $DOCKERHUB_USERNAME/${baseimage}
+    # docker pull ${kube_image_repo}/${baseimage}
+    # docker tag ${kube_image_repo}/${baseimage} $DOCKERHUB_USERNAME/${baseimage}
+    # docker push $DOCKERHUB_USERNAME/${baseimage}
+
+    docker pull --platform linux/amd64 ${kube_image_repo}/${baseimage}
+    docker tag ${kube_image_repo}/${baseimage} $DOCKERHUB_USERNAME/${baseimage}-amd64
+    docker push $DOCKERHUB_USERNAME/${baseimage}-amd64
 done

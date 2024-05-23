4c1a1a844cc01355c9f3b1ed003fe1d1e0461cd6
howardlee2010 <howard@mail.ustc.edu.cn>
2024-05-23 13:50:09 +0800

    Create k8s-build-images-get.sh

---
+++ scripts/k8s-build-images-get.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+#
+set -x
+
+kube_image_repo="registry.k8s.io/build-image"
+
+docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWD
+
+for baseimage in `cat k8s-build-images | grep -v ^$ | grep -v ^#`; do
+    echo ${baseimage}
+    # docker pull ${kube_image_repo}/${baseimage}
+    # docker tag ${kube_image_repo}/${baseimage} $DOCKERHUB_USERNAME/${baseimage}
+    # docker push $DOCKERHUB_USERNAME/${baseimage}
+done

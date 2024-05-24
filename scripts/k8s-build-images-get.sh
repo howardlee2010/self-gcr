aa89375eb277236a61ec6ee1d2ec76e7646ce0fb
howardlee2010 <howard@mail.ustc.edu.cn>
2024-05-24 09:28:56 +0800

    Update k8s-build-images-get.sh

--- scripts/k8s-build-images-get.sh
+++ scripts/k8s-build-images-get.sh
@@ -17,28 +17,37 @@ docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWD
 #     docker push $DOCKERHUB_USERNAME/${baseimage}
 # done
 
-docker pull --platform linux/amd64 registry.k8s.io/build-image/go-runner:v2.3.1-go1.21.9-bookworm.0
-docker tag registry.k8s.io/build-image/go-runner:v2.3.1-go1.21.9-bookworm.0 $DOCKERHUB_USERNAME/go-runner-amd64:v2.3.1-go1.21.9-bookworm.0
-docker push $DOCKERHUB_USERNAME/go-runner-amd64:v2.3.1-go1.21.9-bookworm.0
+# docker pull --platform linux/amd64 registry.k8s.io/build-image/go-runner:v2.3.1-go1.21.9-bookworm.0
+# docker tag registry.k8s.io/build-image/go-runner:v2.3.1-go1.21.9-bookworm.0 $DOCKERHUB_USERNAME/go-runner-amd64:v2.3.1-go1.21.9-bookworm.0
+# docker push $DOCKERHUB_USERNAME/go-runner-amd64:v2.3.1-go1.21.9-bookworm.0
+
+# sleep 2
+
+# docker pull --platform linux/arm64 registry.k8s.io/build-image/go-runner:v2.3.1-go1.21.9-bookworm.0
+# docker tag registry.k8s.io/build-image/go-runner:v2.3.1-go1.21.9-bookworm.0 $DOCKERHUB_USERNAME/go-runner-arm64:v2.3.1-go1.21.9-bookworm.0
+# docker push $DOCKERHUB_USERNAME/go-runner-arm64:v2.3.1-go1.21.9-bookworm.0
+
+# sleep 2
+
+# docker pull --platform linux/amd64 registry.k8s.io/build-image/distroless-iptables:v0.4.7
+# docker tag registry.k8s.io/build-image/distroless-iptables:v0.4.7 $DOCKERHUB_USERNAME/distroless-iptables-amd64:v0.4.7
+# docker push $DOCKERHUB_USERNAME/distroless-iptables-amd64:v0.4.7
+
+# sleep 2
+
+# docker pull --platform linux/arm64 registry.k8s.io/build-image/distroless-iptables:v0.4.7
+# docker tag registry.k8s.io/build-image/distroless-iptables:v0.4.7 $DOCKERHUB_USERNAME/distroless-iptables-arm64:v0.4.7
+# docker push $DOCKERHUB_USERNAME/distroless-iptables-arm64:v0.4.7
+
+docker pull --platform linux/amd64 registry.k8s.io/build-image/setcap:bookworm-v1.0.2
+docker tag registry.k8s.io/build-image/setcap:bookworm-v1.0.2 $DOCKERHUB_USERNAME/setcap-amd64:bookworm-v1.0.2
+docker push $DOCKERHUB_USERNAME/setcap-amd64:bookworm-v1.0.2
 
 sleep 2
 
-docker pull --platform linux/arm64 registry.k8s.io/build-image/go-runner:v2.3.1-go1.21.9-bookworm.0
-docker tag registry.k8s.io/build-image/go-runner:v2.3.1-go1.21.9-bookworm.0 $DOCKERHUB_USERNAME/go-runner-arm64:v2.3.1-go1.21.9-bookworm.0
-docker push $DOCKERHUB_USERNAME/go-runner-arm64:v2.3.1-go1.21.9-bookworm.0
-
-sleep 2
-
-docker pull --platform linux/amd64 registry.k8s.io/build-image/distroless-iptables:v0.4.7
-docker tag registry.k8s.io/build-image/distroless-iptables:v0.4.7 $DOCKERHUB_USERNAME/distroless-iptables-amd64:v0.4.7
-docker push $DOCKERHUB_USERNAME/distroless-iptables-amd64:v0.4.7
-
-sleep 2
-
-docker pull --platform linux/arm64 registry.k8s.io/build-image/distroless-iptables:v0.4.7
-docker tag registry.k8s.io/build-image/distroless-iptables:v0.4.7 $DOCKERHUB_USERNAME/distroless-iptables-arm64:v0.4.7
-docker push $DOCKERHUB_USERNAME/distroless-iptables-arm64:v0.4.7
-
+docker pull --platform linux/arm64 registry.k8s.io/build-image/setcap:bookworm-v1.0.2
+docker tag registry.k8s.io/build-image/setcap:bookworm-v1.0.2 $DOCKERHUB_USERNAME/setcap-arm64:bookworm-v1.0.2
+docker push $DOCKERHUB_USERNAME/setcap-arm64:bookworm-v1.0.2
 
 
 
